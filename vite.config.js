2b238e2c25e0b76099ebf33d4629d4c0e772864c
satyam navdiya <satyamnavdiya@gmail.com>
2025-04-18 18:00:59 +0530

    change main branch get ready for deployment and merge

--- vite.config.js
+++ vite.config.js
@@ -6,44 +6,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      manifest: {
-        name: "RTSV - SDA Dimond Hospital",
-        short_name: "Dimond Hospital",
-        description: "Hospital Management App",
-        theme_color:"#325082",
-        background_color: "#fff",
-        display: "standalone",
-        start_url: "/diamondhospital",
-        icons: [
-          {
-            src: "/diamondhospital/icons/icon-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/diamondhospital/icons/icon-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
-      workbox: {
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/satyamnavdiya\.github\.io\/Hospital\/.*/,
-            handler: "NetworkFirst",
-            options: {
-              cacheName: "hospital-cache",
-            },
-          },
-        ],
-      },
-    }),
-  ],
-  base: '/diamondhospital',
+  plugins: [tailwindcss(), react(),],
+  base: '/',
   optimizeDeps: {
     exclude: ['lucide-react', 'ogl'],
   },
