6befa08c671116f74b7520137e5ac22e85199a28
satyam navdiya <satyamnavdiya@gmail.com>
2025-04-19 19:47:52 +0530

    merge with branch dev

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { VitePWA } from "vite-plugin-pwa";
+// import { VitePWA } from "vite-plugin-pwa";
 import tailwindcss from '@tailwindcss/vite'
 
 
@@ -9,6 +9,6 @@ export default defineConfig({
   plugins: [tailwindcss(), react(),],
   base: '/',
   optimizeDeps: {
-    exclude: ['lucide-react', 'ogl'],
+    exclude: ['lucide-react', 'ogl', 'framer-motion'],
   },
 })
