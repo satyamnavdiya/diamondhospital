b034993a9956a576d1aa2424e348ac1cebfd9186
satyam navdiya <satyamnavdiya@gmail.com>
2025-05-15 18:55:38 +0530

    remove cerifications

--- src/Components/Homepage/Certifications.jsx
+++ src/Components/Homepage/Certifications.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import '../CSS/Homapage.css';
 
 function Certifications() {
     return (
         <>
-            <div className='div-center w-[80%] pt-10 max-[640px]:pt-0 max-sm:pt-8'>
+            <section className='certifications'>
+                <div className='div-center w-[80%] pt-10 max-[640px]:pt-0 max-sm:pt-8'>
                 <div className='text-center'>
                     <span className='bg-teal-500 text-white rounded-xl text-center py-1 px-2'>Our Achievement</span>
                     <h1 className='cer-heading pt-2'><span className="cer-gradient">Certifications</span></h1>
@@ -16,6 +18,7 @@ function Certifications() {
                     <div className='p-5 hover:shadow-2xl transition-all rounded-2xl hover:border-teal-500 hover:border-2'><img src="/IMG/certifications.jpg" alt="" /></div>
                 </div>
             </div>
+            </section>
         </>
     )
 }
